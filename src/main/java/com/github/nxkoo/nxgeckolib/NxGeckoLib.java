38f788de751dee8f8558db9de642330b192097d4
NykooX <nykoofy@proton.me>
2024-07-30 20:58:28 -0300

    ✨ | feat - Setup Geckolib

--- src/main/java/com/github/nxkoo/nxgeckolib/NxGeckoLib.java
+++ src/main/java/com/github/nxkoo/nxgeckolib/NxGeckoLib.java
@@ -1,9 +1,6 @@
 package com.github.nxkoo.nxgeckolib;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -18,40 +15,26 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("nxgeckolib")
 public class NxGeckoLib {
 
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public NxGeckoLib() {
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("NxGeckoLib is started...");
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-    }
+    private void doClientStuff(final FMLClientSetupEvent event) {}
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
         InterModComms.sendTo("nxgeckolib", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
@@ -62,21 +45,11 @@ public class NxGeckoLib {
                 collect(Collectors.toList()));
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        LOGGER.info("NxGeckoLib on server started...");
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+    public static class RegistryEvents {}
 }
---
+++ src/main/java/com/github/nxkoo/nxgeckolib/proxy/CommonProxy.java
@@ -0,0 +1,3 @@
+package com.github.nxkoo.nxgeckolib.proxy;
+
+public class CommonProxy {}
